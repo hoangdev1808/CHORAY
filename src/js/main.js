4888cb85e6398ea7c199fccb62aaf57bf3366470
hoangdev1808 <59635075+hoangdev1808@users.noreply.github.com>
2020-09-15 14:00:10 +0700

    upup

---
+++ src/js/main.js
@@ -0,0 +1,55 @@
+const toggleMenuMobile = () => {
+	$(".toggle-menu").on("click", function() {
+		$(this).toggleClass("active");
+		$(this).siblings(".main-nav").toggleClass("active");
+		$("body").toggleClass("disabled");
+	});
+}
+function moveNavitem() {
+	if ($(window).width() <= 1024) {
+		$('.icon > #desktop').appendTo('nav .nav-mobile');
+	} else {
+		$('.icon > #desktop').appendTo('.main-nav>.nav .icon');
+	}
+}
+
+//Slide banner
+function CrMainBanner() {
+	var swiperhomebanner = new Swiper('.Cr-banner__slide', {
+		loop: true,
+		speed: 1000,
+		grabCursor: true,
+		watchSlidesProgress: true,
+		mousewheelControl: true,
+		keyboardControl: true,
+		effect: 'fade',
+		autoplay: {
+			delay: 3500,
+			disableOnInteraction: false
+		},
+		navigation: {
+			nextEl: ".Cr-banner__slide .swiper-button-next",
+			prevEl: ".Cr-banner__slide .swiper-button-prev"
+		}
+
+	});
+}
+//Check banner
+const checkLayoutBanner = () => {
+	const pagesBanner = $("#page-banner");
+	const breadcrumb = $('.global-breadcrumb');
+	const heightHeader = $("header").outerHeight();
+	const mainBanner = $("#home-banner");
+
+	if (mainBanner.length < 1) {
+		pagesBanner.css('padding-top', heightHeader);
+	} else {
+		mainBanner.css('padding-top', heightHeader);
+	}
+};
+document.addEventListener('DOMContentLoaded', () => {
+	toggleMenuMobile();
+	moveNavitem();
+	CrMainBanner();
+	checkLayoutBanner()
+});
